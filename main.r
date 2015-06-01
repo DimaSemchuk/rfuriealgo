bb284215e2f892cdcec42428529c6daddc5facfc
DimaSemchuk <dima.september@gmail.com>
2015-06-01 04:52:29 +0300

    WAV is loaded, and we got rate, channels, etc in readable format (with xe-y numbers)

--- main.r
+++ main.r
@@ -1,1 +1,10 @@
-print("hello");
+require(tuneR)
+
+#variables block
+sample <- readWave("sample/test.wav")
+sampleLeftChannel <- sample@left / 2^(sample@bit -1)
+sampleRightChannel <- sample@right / 2^(sample@bit -1)
+
+#sampling
+timeArray = (1:length(sampleLeftChannel)) / sample@samp.rate
+str(timeArray)
