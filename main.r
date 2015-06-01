d69be20eb2083c7a6610126fa2d04103bd3d213a
DimaSemchuk <dima.september@gmail.com>
2015-06-01 05:47:05 +0300

    plotting frequency, BUT file is under root folder, so please MOVE IT and solve folder problem TODO

--- main.r
+++ main.r
@@ -1,10 +1,45 @@
 require(tuneR)
 
-#variables block
-sample <- readWave("sample/test.wav")
-sampleLeftChannel <- sample@left / 2^(sample@bit -1)
-sampleRightChannel <- sample@right / 2^(sample@bit -1)
+#
+# variables block
+#
+sample <- readWave("test.wav")
+leftChannel <- sample@left / 2^(sample@bit -1)
+rightChannel <- sample@right / 2^(sample@bit -1)
+leftChannelLength = length(leftChannel)
+leftChannelLabels <- leftChannel[seq(0, leftChannelLength,500)]
 
-#sampling
-timeArray = (1:length(sampleLeftChannel)) / sample@samp.rate
-str(timeArray)
+timeArray <- ((0:(length(leftChannel) - 1)) / sample@samp.rate) * 1000
+timeArray <- timeArray[seq(0, length(timeArray), 500)]
+
+#
+# plotting initial data
+#
+#plot(timeArray, leftChannelLabels, type='l', col='black', xlab='Time (ms)', ylab='Amplitude')
+
+#
+# Get FFT points
+#
+
+leftChannelPoints <- fft(leftChannel)
+nUniquePts <- ceiling((leftChannelLength + 1) / 2)
+
+# select just the first half since the second half 
+# is a mirror image of the first
+# and take the absolute value, or the magnitude 
+leftChannelPoints <- ((abs(leftChannelPoints[1:nUniquePts])) / leftChannelLength) ^ 2
+leftChannelPointsLength = length(leftChannelPoints)
+
+# multiply by two (see technical document for details)
+# odd nfft excludes Nyquist point
+if (leftChannelLength %% 2 > 0) {
+  # we've got odd number of points fft
+  leftChannelPoints[2:leftChannelPointsLength] <- leftChannelPoints[2:leftChannelPointsLength]*2
+} else {
+  # we've got even number of points fft
+  leftChannelPoints[2: (leftChannelPointsLength -1)] <- leftChannelPoints[2: (leftChannelPointsLength -1)]*2
+}
+
+freqArray <- (0:(nUniquePts - 1)) * (sample@samp.rate / leftChannelLength*500) #  create the frequency array 
+plot(freqArray/1000, 10*log10(leftChannelPoints), type='l', col='black', xlab='Frequency (kHz)', ylab='Power (dB)')
+
